f409157ac7e8102dd0ddd896163edef7b65ed9ac
BarnaSanyi <sbarnabas0502@gmail.com>
2025-12-04 21:03:08 +0100

    A főprogram alapjának megírása, adatbázissal való összekötés megvalósítása és saját immutable létrehozása.

---
+++ Hazi/RoadMaintenanceApp/RoadMaintenanceApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using RoadMaintenanceApp.Data;
+using RoadMaintenanceApp.Models;
+using RoadMaintenanceApp.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var context = new AppDbContext();
+        var service = new RoadService(context);
+
+        while (true)
+        {
+            Console.WriteLine("\n--- Út Karbantartó Rendszer ---");
+            Console.WriteLine("1. Új út felvétele");
+            Console.WriteLine("2. Út módosítása");
+            Console.WriteLine("3. Út törlése");
+            Console.WriteLine("4. Keresés és Listázás (JSON Exporttal)");
+            Console.WriteLine("5. Kilépés");
+            Console.Write("Válasszon: ");
+
+            var choice = Console.ReadLine();
+
+            try
+            {
+                switch (choice)
+                {
+                    case "1":
+                        //Új utat ad hozzá
+                        break;
+                    case "2":
+                        //Út módosítása
+                        break;
+                    case "3":
+                        //Út törlése
+                        break;
+                    case "4":
+                        //Keresés és listázás JSON exporttal
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Érvénytelen választás.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"HIBA TÖRTÉNT: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Hazi/RoadMaintenanceApp/RoadMaintenanceApp/Services/RoadService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoadMaintenanceApp.Data;
+using RoadMaintenanceApp.Models;
+using System.Text.Json;
+
+namespace RoadMaintenanceApp.Services
+{
+    public class RoadService
+    {
+        private readonly AppDbContext _context;
+
+        public RoadService(AppDbContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+    }
+}
